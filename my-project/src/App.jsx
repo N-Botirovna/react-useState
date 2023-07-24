a4caf5adf87918a98c7c462d305aa21fee070192
Nazimjonovna <sturayeva000@gmail.com>
2023-07-24 13:29:00 +0300

    add

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -2,41 +2,45 @@ import { useState } from 'react'
 
 
 function App() {
-  
+
   let [type, setType] = useState("Positive");
-  const handleClick1 = ()=>{
+  const handleClick1 = () => {
     setType("Positive");
   }
-  const handleClick2 = ()=>{
+  const handleClick2 = () => {
     setType("Negative");
   }
-  const handleClick3 = (e)=>{
+  const handleClick3 = (e) => {
     setType("Neutral");
   }
-  const handleChange = (e) =>{
-    if(e.target.value == "1"){
+  const handleChange = (e) => {
+    if (e.target.value == "1") {
       setType("Positive");
     }
-    else if(e.target.value == "2"){
+    else if (e.target.value == "2") {
       setType("Negative");
     }
-    else if(e.target.value == "3"){
+    else if (e.target.value == "3") {
       setType("Neutral");
     }
   }
   return (
     <>
-      <div className="inputDiv">
-        <input type="number" name="input" id="input" placeholder='Enter tab index' onClick={handleChange}/>
-        <label htmlFor='input' onClick={handleChange} >Change tab</label>
-        
+      <div className=" bg-blue-400">
+        <div className=" bg-white mx-auto px-3 py-4 ">
+          <div className="inputDiv">
+            <input type="number" name="input" id="input" placeholder='Enter tab index' onClick={handleChange} />
+            <label htmlFor='input' onClick={handleChange} >Change tab</label>
+
+          </div>
+
+          <button className='color-red' onClick={handleClick1}>Positive</button>
+          <button onClick={handleClick2}>Negative</button>
+          <button onClick={handleClick3}>Neutral</button>
+
+          <p>{type} content</p>
+        </div>
       </div>
-      
-      <button onClick={handleClick1}>Positive</button>
-      <button onClick={handleClick2}>Negative</button>
-      <button onClick={handleClick3}>Neutral</button>
-      
-      <p>{type} content</p>
     </>
   )
 }
